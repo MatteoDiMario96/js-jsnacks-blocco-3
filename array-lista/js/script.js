c04a1825bb2a12762c72354c4914d4f8803509cb
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-14 16:23:11 +0200

    Aggiunto ciclo for per aumentare sempre di uno nell indice dell'array. e gli ho chiesto SE nell'array frigorifero nel tuo i( che quindi cresce con il ciclo e quindi cambia valore) è === "cocomero" l'elemento che io stavo cercando. Se si stampa ce l'ho se è diverso da cocomenro non fai nulla altrimenti ( cioè se non ho niente) dimmi di andarlo a comprare

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -12,3 +12,15 @@ const frigorifero = [
 
 frigorifero.push("pesca")
 
+for(let i = 0; i < frigorifero.length; i++){
+    if (frigorifero[i] === "cocomero"){
+        console.log("Non ti preoccupare, abbiamo il cocomero!")
+    } else if (frigorifero[i] !== "cocomero"){
+
+    } else {
+        console.log("Mi dispiace, devi uscire a comprarlo!")
+    }
+} 
+
+// Aggiunto ciclo for per aumentare sempre di uno nell indice dell'array.
+// e gli ho chiesto SE nell'array frigorifero nel tuo i( che quindi cresce con il ciclo e quindi cambia valore) è === "cocomero" l'elemento che io stavo cercando. Se si stampa ce l'ho se è diverso da cocomenro non fai nulla altrimenti ( cioè se non ho niente) dimmi di andarlo a comprare 
