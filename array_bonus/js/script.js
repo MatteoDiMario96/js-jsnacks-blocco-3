c854bc2bfc1a8ffe368c72c828bc2c79a4b6b616
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-14 18:04:54 +0200

    Creato un ciclo che dice che l'indice deve partire dalla lunghezza dell'arreyA +1 e finisce nella lunghezza dell'arreyB +1. l'Indice la pusho dentro l'arreyA. Metto in console i due arrey.

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -3,3 +3,7 @@ const arrayA = [1, 2, 3, 4, 5, 6, 7, 8];
 const arrayB = ["Matteo", "Sara", "Giovanni", "Paolo", "Alfredo", "Paola", "Mirko", "Samantha", "Riccardo", "Arnaldo"];
 
 
+for(let i = arrayA.length + 1; i < arrayB.length + 1; i++){
+    arrayA.push(i)
+}
+console.log(arrayA,arrayB)
