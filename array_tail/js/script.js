36b4b49b53bcaa5b6ef488c5786728a7a9215356
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-14 17:10:45 +0200

    Creato array vuoto. Creato prompt per far decidere numero di elementi da inserire nell'arrey. Funzione che se il numero è piu piccolo di 5, più grande di 500 o isNan lo imposto io a 50, e comunico in console che il valore è stato impostato a 50.

---
+++ array_tail/js/script.js
@@ -0,0 +1,9 @@
+const arrayVuoto = [];
+
+let NumeroUtente = Number.parseInt(prompt("Da quanti elementi vuoi che sia costituito un array !"), 10)
+
+if(NumeroUtente < 5 || NumeroUtente > 500 || isNaN(NumeroUtente)){
+    NumeroUtente = 50;
+    console.log("Mi dispiace ti ho dato come valore io 50")
+}
+
