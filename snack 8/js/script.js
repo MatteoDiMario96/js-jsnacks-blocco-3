396d095a0d894be88128e3b0174650dae96eb79c
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-15 11:56:12 +0200

    intanto phuso

---
+++ snack 8/js/script.js
@@ -0,0 +1,9 @@
+// Snack9 Crea un array vuoto. Chiedi fino a che l'array non ha 6 numeri all'utente di inserire un numero,
+//  se è dispari inseriscilo nell'array.
+
+const arrayVuoto = [];
+while(arrayVuoto.length(6)){
+    let numeroUtente = Number.parseInt(prompt("Type a number"), 10);
+}
+
+if
