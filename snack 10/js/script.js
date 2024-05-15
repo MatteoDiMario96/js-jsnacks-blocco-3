ce3fa3295a2abc04aa7aa814eda375ae30714a69
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-15 12:38:38 +0200

    Pseudocodice

---
+++ snack 10/js/script.js
@@ -0,0 +1,20 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+// - Crea un Array vuoto 
+
+// - Chiedi all'utente un numeor da inserire dentro all'array 
+
+// - Continuo a chiedere numeri finche la loro somma non arriva almeno a 50. 
+
+
+
+
+
+
+
+
+
+
+
+
--- snack 8/js/script.js
+++ snack 8/js/script.js
@@ -1,19 +1,19 @@
 // Snack9 Crea un array vuoto. Chiedi fino a che l'array non ha 6 numeri all'utente di inserire un numero,
 //  se è dispari inseriscilo nell'array.
 
-// const arrayVuoto = [];
+const arrayVuoto = [];
 
+let numeroUtente;
+let numeroUtenteDisparo = numeroUtente % 2 === 1;
 
-// let numeroUtenteDisparo = numeroUtente % 2 === 1;
+while(arrayVuoto.length < 6){
+    numeroUtente = Number.parseInt(prompt("Type a number"), 10);
+}
 
-// while(arrayVuoto.lenght < 6){
-//     numeroUtente = Number.parseInt(prompt("Type a number"), 10);
-// }
-
-// if(numeroUtenteDisparo){
-//     arrayVuoto.push(numeroUtente)
-// }
-// console.log(arrayVuoto)
+if(numeroUtenteDisparo){
+    arrayVuoto.push(numeroUtente)
+}
+console.log(arrayVuoto)
 
 
 // ----DA RIFARE----
