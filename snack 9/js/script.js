e0f556b47a54e32de485f0b92545ecaacc6a9f6d
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-15 12:16:02 +0200

    pseudocodice

---
+++ snack 9/js/script.js
@@ -0,0 +1,9 @@
+// Crea due array che hanno un numero di elementi diversi. 
+// Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+// - Crea un array tot elementi
+
+// - Crea un array tot elementi + 1 
+
+// -Crea un ciclo while fino a quando array con meno elementi ha stessi elementi di quello più lungo
+//     - Aggiungo elementi a quello più corto 
