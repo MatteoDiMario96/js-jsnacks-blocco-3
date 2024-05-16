5b596d4c34d95a9c90aec26daf6c1fbb2fc83d6e
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-16 11:37:40 +0200

    credo giusto.

--- snack 12/js/script.js
+++ snack 12/js/script.js
@@ -18,3 +18,19 @@
 // - Chiedo all'utente la parolaUtenteB
 
 // - Richiamo la funzione con argomento ( parolaUtenteA e parolaUtenteB)
+
+
+function lunghezzaParole (parolaA, parolaB){
+    if(parolaA.lenght == parolaB.lenght){
+        console.log(parolaA, parolaB)
+    } else if (parolaA > parolaB){
+        console.log(parolaA)
+    } else if(parolaB > parolaA){
+        console.log(parolaB)
+    }
+}
+
+const parolaUtenteA = prompt("Dammi una parola");
+const parolaUtenteB = prompt("Dammi una parola");
+
+lunghezzaParole(parolaUtenteA,parolaUtenteB)
