63cf0dc5cb79b73b285ff98b5dc82f91cd63a2e2
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-16 11:31:07 +0200

    pseudocodice, con il mal di testa che mi disabilita.

---
+++ snack 12/js/script.js
@@ -0,0 +1,20 @@
+// Snack5
+// Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+// - Mi creo una funzione con argomento parolaA e parolaB
+//     - SE le parole sono uguali 
+//         - Stampa entrambe le parole 
+    
+//     - Altrimenti SE la parolaA è piu lunga 
+//         -Scrivi lei 
+    
+//     - Altrimenti SE la parolaB è più lunga 
+//         - Scrivi lei 
+
+
+
+// - Chiedo all'utente la parolaUtenteA
+
+// - Chiedo all'utente la parolaUtenteB
+
+// - Richiamo la funzione con argomento ( parolaUtenteA e parolaUtenteB)
