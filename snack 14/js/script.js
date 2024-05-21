fb34c8bc0df3ff5bb6182cfa0c0ef8fcca73ded3
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-21 12:33:27 +0200

    Credo

---
+++ snack 14/js/script.js
@@ -0,0 +1,11 @@
+// ! Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
+
+const secondUtent = Number.parseInt(prompt("Dimmi dei secondi da attendere!"), 10);
+
+let clock = setInterval (function(){
+    secondUtent--;
+    if(secondUtent <= 0){
+        clearInterval(clock);
+        alert("Interrompi Timer");
+    }
+})
