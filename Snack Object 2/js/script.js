8b3b3c79e679a6d4458a1bb1b16853c0e0297bcf
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-23 12:08:36 +0200

    Creazione array

---
+++ Snack Object 2/js/script.js
@@ -0,0 +1,82 @@
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
+
+const automobili = [
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "benzina"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "diesel"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "gpl"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "elettrico"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "metano"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "benzina"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "diesel"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "gpl"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "elettrico"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "metano"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "benzina"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "diesel"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "gpl"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "elettrico"
+    },
+    {
+        marca: "opel",
+        modello: "corsa",
+        alimentazione: "metano"
+    },
+]
