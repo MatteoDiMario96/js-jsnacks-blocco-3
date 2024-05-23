025c063160e113d32586dd1eccfa334259494a0c
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-23 12:49:42 +0200

    push.

--- Snack Object 3/js/script.js
+++ Snack Object 3/js/script.js
@@ -58,13 +58,13 @@ const persone = [
 
 let nuovoArrayDiPersone = persone.map((elemento) => {
     if(elemento.eta > 18){
-        return `Sono ${elemento.nome} ${elemento.cognome} con quest'età puoi guidare ${elemento.eta} --`
+        return `Sono ${elemento.nome} ${elemento.cognome} e con quest'età puoi guidare, ho ${elemento.eta} --`
     }
 });
 
 nuovoArrayDiPersone += persone.map((elemento) => {
     if(elemento.eta < 18){
-        return `Sono ${elemento.nome} ${elemento.cognome} e con quest'età mi dispiace ma non puoi guidare ${elemento.eta}`
+        return `Sono ${elemento.nome} ${elemento.cognome} e con quest'età mi dispiace ma non puoi guidare, ho ${elemento.eta}`
     }
 });
 
