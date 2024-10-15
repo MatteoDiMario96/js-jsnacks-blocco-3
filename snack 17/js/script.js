849abbcda873cae4b8604d427f0c2f430708f59a
MatteoDiMario96 <dm.matteo96@live.it>
2024-10-15 18:46:45 +0200

    script snack 17

--- snack 17/js/script.js
+++ snack 17/js/script.js
@@ -10,3 +10,30 @@ function wordReverse(str){
     return wordSplitted.join(' ');
 }
 
+
+function narcissistic(value) {
+  
+    //Trasform into string 
+    const valueString = value.toString();
+    
+    //Split in array 
+    const valueSplitted = valueString.split('');
+    
+    //Count the digits
+    const valueDigits = valueSplitted.length;
+    
+    // Variable for number elevated
+    let count = 0;
+    
+    //Elevation to power added at count
+    for(let i = 0; i < valueDigits; i++){
+      count += Math.pow(valueSplitted[i],  valueDigits);
+    }
+    
+    //Condition for Narcissistic numbers
+    if(value === count){
+      return true;
+    }else{
+      return false
+    }
+  }
